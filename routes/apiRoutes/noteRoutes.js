b37e911c4014c8995bfe84e6e8a5aca858f69a3d
hienm9 <98295316+hienm9@users.noreply.github.com>
2022-05-02 21:14:56 -0400

    added comments to the post request

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -2,6 +2,8 @@
 const router = require('express').Router();
 const savedNotes = require('../../db/db.json');
 const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+
 
 // GET request
 //GET /api/notes should read the db.json file and return all saved notes as JSON.
@@ -12,8 +14,12 @@ router.get('/notes', function (req, res) {
 // POST request
 router.post('/notes', (req, res) => {
   var note = req.body;
+  // add an id to note
+  note.id = uuidv4(); 
   console.log(note);
+  //push note to saveNotes 
   savedNotes.push(note);
+  // permenantly save updated note
   fs.writeFile('./db/db.json', JSON.stringify(savedNotes),function(err){
       if(err) throw err;
       res.json(note);
